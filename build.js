8dd5e9b46830777782f9ace995ace947a86ae3d1
Daniel Elebash <danelebash@hotmail.com>
2016-09-27 18:00:05 -0400

    Initial commit

---
+++ build.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+// This is a wrapper script around electron-packager and other tasks for output.
+var exec = require('child_process').execSync,
+	rimraf = require('rimraf'),
+	args = require('yargs').argv,
+	out = args.out || './builds',
+	arch = args.arch || process.arch,
+	platform = args.platform || process.platform,
+	productName = args.name || require('./package.json').productName,
+	nodeVersion = parseFloat(process.version.substr(1, 5)),
+	adjustedOut = out + '/' + platform + '/' + arch,
+	asar = !!args.asar;
+
+if (nodeVersion < 0.12) {
+	process.stderr.write('This project requires node >= 0.12');
+	process.exit(1);
+} else {
+  process.stdout.write('Preparing app for platform ' + platform + ' ' + arch + '\n');
+}
+
+var electronPackager = [
+	'node',
+	'./node_modules/electron-packager/cli.js',
+	'.',
+	productName,
+	'--prune',
+	'--arch=' + arch,
+	'--version=0.27.3',
+	'--out=' + adjustedOut,
+	'--platform=' + platform,
+	'--ignore=./builds/',
+	'--ignore=./gulpfile.js'
+];
+
+if (asar) {
+	electronPackager.push('--asar');
+}
+
+process.stdout.write('Removing ' + adjustedOut + '\n');
+rimraf.sync(adjustedOut);
+
+process.stdout.write('Executing ' + electronPackager.join(' ') + '\n');
+exec(electronPackager.join(' '), {
+	cwd: __dirname
+});
---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+'use strict';
+
+require('longjohn'); // long stack traces, across async
+
+// Register babel to compile all javascript.
+// NOTE: By default all requires to node_modules will be ignored.
+// NOTE2: Electron already enables ES6, so this is only required from development files that require our own ES6 modules.
+require('babel/register');
+require('require-dir')('build/tasks');
